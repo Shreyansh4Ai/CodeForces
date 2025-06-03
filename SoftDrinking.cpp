0c34e9dabd013edfd96b82760278bca8bdb8023f
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-03 23:44:48 +0530

    Create SoftDrinking.cpp

---
+++ SoftDrinking.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n , k , l , c , d , p , nl ,np;
+cin>> n >> k >> l >> c >> d >> p >> nl >> np;
+
+int toast = (k*l)/nl ; 
+int limes = c*d  ;
+int salt = p/np;
+cout<< min({toast , limes , salt} ) / n <<endl;
+}
