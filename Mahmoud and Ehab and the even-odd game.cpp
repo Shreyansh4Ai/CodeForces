6dcafa7565d04b0571c6c7db0acc741bed45fe51
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-12 23:10:12 +0530

    looks crepy but easy

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+long long int n ;
+cin>> n ;
+ // if(n == 1 ) cout<<"Ehab" << endl;
+  
+  //if (n == 2) cout<<"Mahmoud"<<endl;
+  
+  if(n%2==0) cout<<"Mahmoud"<<endl;
+  else cout<<"Ehab" << endl;
+ 
+}
