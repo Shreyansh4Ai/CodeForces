14b084020a7bb6cf8f64b5a7fdadeefc9852e0dd
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-08-08 23:21:41 +0530

    Create AgainTwentyFive.cpp

---
+++ AgainTwentyFive.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long  ;
+int main() {
+	// your code goes here
+ll n ;
+cin >> n ;
+if( n >=2) cout <<25<<endl;
+else cout <<05<<endl;
+}
