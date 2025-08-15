3984ffcb7df02344292a664ea5ac067e0c5d1bac
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-08-15 23:18:23 +0530

    easy

---
+++ A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    
+        int n , m , k;
+        cin >> n >> m >> k ;
+      
+
+        if( n > m || n > k){
+            cout<<"No"<<endl;
+        }else {
+            cout<<"Yes"<<endl;
+        }
+
+    }
