6e7e3019603d49554785743b5c73c01b6955b49a
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-08-23 11:19:21 +0530

    good

---
+++ A_Olympiad.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    int n ;
+    cin >> n ;
+
+    vector<int> a(n);
+    for(int i = 0 ; i< n ; i++){
+        cin >> a[i];
+    }
+     sort(a.begin(), a.end());
+     int cnt = 0 ;
+    for(int i = 0 ; i<n  ; i++){
+        if(a[i] == 0);
+        else if(a[i+1] != a[i]) {
+          cnt++ ;
+        }
+
+    }
+   cout << cnt<< endl;
+}
