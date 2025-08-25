74a1e6fc8c487c396beef65c79a4f575116d66f2
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-08-25 11:55:58 +0530

    easy ceil in correct way

--- pensAndPencil.cpp
+++ pensAndPencil.cpp
@@ -2,25 +2,22 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int a, b, c, d, k;
-        cin >> a >> b >> c >> d >> k;
-
-        // minimum pens needed
-        int pens = (a + c - 1) / c;
-        // minimum pencils needed
-        int pencils = (b + d - 1) / d;
-
-        if (pens + pencils <= k) {
-            cout << pens << " " << pencils << "\n";
-        } else {
-            cout << -1 << "\n";
-        }
+int t ;
+cin >> t ;
+while(t--){
+    int a , b ,c ,d , k ;
+    cin >> a >> b >> c >> d >> k ;
+    
+    int l = ((a + c - 1 )/ c) ; 
+    int p = ((b+d - 1) / d) ;
+    
+    if(l+p <= k){
+        cout <<l <<" "<< p << endl;
+    }else {
+        cout <<-1<< endl;
     }
-    return 0;
+    
+    
+}
+
 }
