5e3b72830fabb0719013c3bc70bbed21cca908ea
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-09-14 20:54:04 +0530

    good

---
+++ B_Blank_Space.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    int t ;
+    cin >> t ;
+    while(t--){
+        int n ;
+         cin >> n ;
+
+        int arr[n];
+        for(int i = 0 ; i<n ; i++){
+            cin >> arr[i];
+        } 
+        int cnt = 0;
+        int maxLen= 0 ;
+
+        for(int i = 0 ; i< n ; i++){
+
+          if(arr[i] == 0) cnt++;
+          else cnt = 0 ;
+           
+          maxLen = max(maxLen , cnt);
+
+        }
+
+        cout << maxLen << endl;
+
+    }
+}
