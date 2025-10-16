5fff4196d4e0b0da1f1374bf17b689dc0ebdadb5
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-10-16 23:25:46 +0530

    easy

---
+++ EasyProblem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; // Number of test cases
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << (n - 1) << endl; // Number of valid (a, b) pairs
+    }
+    return 0;
+}
