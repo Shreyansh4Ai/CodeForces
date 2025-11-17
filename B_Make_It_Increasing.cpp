ab81e2c648a1184773e9af034aa56412bd4441e6
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-17 23:47:54 +0530

    good

--- B_Make_It_Increasing.cpp
+++ B_Make_It_Increasing.cpp
@@ -12,28 +12,22 @@ int main(){
     for(ll i = 0 ; i<n ; i++){
         cin >> a[i];
     }
+      ll cnt = 0; 
+    for(ll i = n-2  ; i>=0 ; i--){
+        while(a[i] >= a[i+1]){
+            cnt++ ;
+            a[i]/=2 ;
+            if(a[i] == 0 )
+            break ;
 
-    if(a[n-1] == 1 ) cout << -1 << endl;
-    else {
+     }
+       if(a[i] == 0 && a[i+1] == 0 ){
+        cnt = -1 ;
+        break ;    
+        }
 
-         ll cnt = 0 ; 
-           
-         for (int i = n - 2  ; i>= 0 ;i--){
-            while(a[i] >= a[i+1]){
-                cnt++;
-                a[i]/2
-
-            }
-
-
-
-         }
-
-           cout << cnt << endl;
     }
-
-
-
-
+    cout << cnt << endl;
+  
     }
 }
