ff1aa88e16f40a797e21cee736f676ba6a98abc3
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2026-05-19 16:33:46 +0530

    Add initial implementation of B_badboy.cpp

---
+++ Rated 900/B_badboy.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long ;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int t ;
+	cin >> t ;
+	while(t--){
+	
+	ll n , m , i , j ;
+	cin >> n >> m >> i >> j ;
+	
+	
+	cout << 1 <<" "<< 1 <<" "<< n <<" "<< m;
+	cout<<endl;
+	    
+	    
+	    
+	    
+	}
+
+}
